6f70b2c2f19adfdd8b035ffc3aa7e94b1339313a
Rahul <rahulsah7846@gmail.com>
2023-04-16 20:50:10 +0530

    com

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("Welcome to Spotify");
+let songs = [
+    {songName: "Salam-e-Ishq", filepath: "song/1/mp3", coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filepath: "song/1/mp3", coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filepath: "song/1/mp3", coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filepath: "song/1/mp3", coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filepath: "song/1/mp3", coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filepath: "song/1/mp3", coverPath: "covers/1.jpg"}
+]
+ 
+let Audioelement = new Audio('1.mp3');
+// Audioelement.play();
+
